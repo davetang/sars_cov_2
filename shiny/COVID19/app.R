aa8ef5e6942c51e4111c1a56d4bec275e3430d15
davetang <davetingpongtang@gmail.com>
2020-11-14 15:21:19 +0900

    Shiny app table

--- shiny/COVID19/app.R
+++ shiny/COVID19/app.R
@@ -10,21 +10,60 @@
 library(shiny)
 library(tidyverse)
 library(plotly)
+library(DT)
 theme_set(theme_bw())
-time_series_data <- read_csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+time_series_confirmed <- read_csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+time_series_deaths <- read_csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+choices <- unique(time_series_confirmed$`Country/Region`)
+
+prep_table <- function(country){
+    
+    time_series_deaths %>%
+        filter(`Country/Region` == country) %>%
+        select_at(-(1:4)) %>%
+        t() %>%
+        as.data.frame() %>%
+        rownames_to_column(var = "date") %>%
+        mutate(deaths = rowSums(.[-1])) %>%
+        mutate(date = as.Date(date, format = "%m/%d/%y")) %>%
+        mutate(deaths = c(0, diff(deaths))) %>%
+        select(date, deaths) %>%
+        arrange(desc(date)) -> deaths
+    
+    time_series_confirmed %>%
+        filter(`Country/Region` == country) %>%
+        select_at(-(1:4)) %>%
+        t() %>%
+        as.data.frame() %>%
+        rownames_to_column(var = "date") %>%
+        mutate(confirmed = rowSums(.[-1])) %>%
+        mutate(date = as.Date(date, format = "%m/%d/%y")) %>%
+        mutate(confirmed = c(0, diff(confirmed))) %>%
+        select(date, confirmed) %>%
+        arrange(desc(date)) %>%
+        inner_join(y = deaths, by = "date")
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    titlePanel("COVID19 cases"),
+    titlePanel("COVID-19 statistics"),
     
     sidebarLayout(
         sidebarPanel(
-            selectInput("country", "Country/Region:", choices = unique(time_series_data$`Country/Region`))
+            selectInput("country", "Country/Region:", choices = choices, selected = "Sweden"),
+            a("Data source", href="https://github.com/CSSEGISandData/COVID-19")
         ),
 
         mainPanel(
-           plotlyOutput("case_plot")
+            tabsetPanel(type = "tabs",
+                        tabPanel("Plots",
+                                 plotlyOutput("plot_confirmed"),
+                                 plotlyOutput("plot_deaths")
+                        ),
+                        tabPanel("Table",
+                                 dataTableOutput("table"))
+            )
         )
     )
 )
@@ -32,8 +71,8 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$case_plot <- renderPlotly({
-        time_series_data %>%
+    output$plot_confirmed <- renderPlotly({
+        time_series_confirmed %>%
             filter(`Country/Region` == input$country) %>%
             select_at(-(1:4)) %>%
             t() %>%
@@ -44,7 +83,7 @@ server <- function(input, output) {
             mutate(confirmed = c(0, diff(confirmed))) -> df
         
         my_total <- sum(df$confirmed)
-        my_title <- paste0(input$country, ": ", my_total)
+        my_title <- paste0(input$country, " total: ", my_total)
         df$confirmed[df$confirmed < 0] <- 0
         
         ggplot(df, aes(date, confirmed)) +
@@ -54,6 +93,33 @@ server <- function(input, output) {
         
         ggplotly(p)
     })
+    
+    output$plot_deaths <- renderPlotly({
+        time_series_deaths %>%
+            filter(`Country/Region` == input$country) %>%
+            select_at(-(1:4)) %>%
+            t() %>%
+            as.data.frame() %>%
+            rownames_to_column(var = "date") %>%
+            mutate(deaths = rowSums(.[-1])) %>%
+            mutate(date = as.Date(date, format = "%m/%d/%y")) %>%
+            mutate(deaths = c(0, diff(deaths))) -> df
+        
+        my_total <- sum(df$deaths)
+        my_title <- paste0(input$country, " total: ", my_total)
+        df$deaths[df$deaths < 0] <- 0
+        
+        ggplot(df, aes(date, deaths)) +
+            geom_col() +
+            theme(axis.title.x = element_blank()) +
+            labs(y = "Confirmed deaths", title = my_title) -> p
+        
+        ggplotly(p)
+    })
+    
+    output$table <- renderDataTable(
+        datatable(prep_table(input$country))
+    )
     
 }
 
