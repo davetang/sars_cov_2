eacfd6edad6c3db0ee8131ddb65ce274a77cad06
davetang <davetingpongtang@gmail.com>
2021-08-22 22:11:33 +0900

    Generate consensus for a lineage

--- analysis/msa/README.md
+++ analysis/msa/README.md
@@ -1,14 +1,4 @@
 ## README
 
-Align with MAFFT.
-
-```bash
-mafft --retree 2 --inputorder genomic.fna > genomic.mafft.fna
-```
-
-Create consensus with cons.
-
-```bash
-cons -sequence genomic.mafft.fna -outseq consensus.fa
-```
+The script `consensus.sh` will extract the genomic sequences for a lineage, create a new FASTA with only unique sequences, produce a multiple sequence alignment (MSA) with MAFFT, and finally output a consensus sequence from the MSA using Cons.
 
---
+++ analysis/msa/consensus.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+variant=P.1
+
+rand=$$$RANDOM
+tmpdir=/tmp/${rand}
+
+if [[ -d ${tmpdir} ]]; then
+   >&2 echo ${tmpdir} already exists!
+   exit 1
+else
+   mkdir ${tmpdir}
+fi
+
+trap "rm -rf ${tmpdir}" SIGINT SIGTERM
+
+if [[ ! -d ${variant} ]]; then
+   mkdir ${variant}
+else
+   >&2 echo Directory ${variant} already exists
+fi
+
+if [[ ! -e ${variant}/genomic.fna ]]; then
+   >&2 echo Unzipping ../../raw/variants/SARS-CoV-2-${variant}.20210819.zip
+   unzip -p ../../raw/variants/SARS-CoV-2-${variant}.20210819.zip ncbi_dataset/data/genomic.fna > ${tmpdir}/genomic.fna
+   mv ${tmpdir}/genomic.fna ${variant}
+else
+   >&2 echo ${variant}/genomic.fna already exists
+fi
+
+if [[ ! -e ${variant}/genomic.uniq.fa ]]; then
+   >&2 echo Generating ${variant}/genomic.uniq.fa
+   ../../script/unique_seq.pl -f ${variant}/genomic.fna > ${tmpdir}/genomic.uniq.fa
+   mv ${tmpdir}/genomic.uniq.fa ${variant}
+else
+   >&2 echo ${variant}/genomic.uniq.fa already exists
+fi
+
+if [[ ! -e ${variant}/genomic.uniq.mafft.fa ]]; then
+   >&2 echo Generating ${variant}/genomic.uniq.mafft.fa
+   mafft --retree 2 --inputorder ${variant}/genomic.uniq.fa > ${tmpdir}/genomic.uniq.mafft.fa
+   mv ${tmpdir}/genomic.uniq.mafft.fa ${variant}
+else
+   >&2 echo ${variant}/genomic.uniq.mafft.fa already exists
+fi
+
+if [[ ! -e ${variant}/consensus.fa ]]; then
+   >&2 echo Generating ${variant}/consensus.fa
+   cons -sequence ${variant}/genomic.uniq.mafft.fa -outseq ${tmpdir}/consensus.fa
+   mv ${tmpdir}/consensus.fa ${variant}
+else
+   >&2 echo ${variant}/consensus.fa already exists
+fi
+
+>&2 echo Done!
+
