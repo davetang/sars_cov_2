1a36943ecb66879010afbd634df52fb8d70c63c2
davetang <davetingpongtang@gmail.com>
2021-08-22 22:08:47 +0900

    Output unique sequences

--- script/unique_seq.pl
+++ script/unique_seq.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/env perl
+#
+# Output unique sequences; FASTA identifiers are lost
+#
 
 use strict;
 use warnings;
 use Getopt::Std;
 
 my %opts = ();
-getopts('i:h:f:', \%opts);
+getopts('h:f:w:', \%opts);
 
 if ($opts{'h'} ||
     !exists $opts{'f'}
@@ -13,22 +16,38 @@ if ($opts{'h'} ||
    usage();
 }
 
-my $infile = $opts{'i'};
-my $fasta_file = $opts{'f'};
+my $wrap_len = 60;
+if (exists $opts{'w'}){
+   $wrap_len = $opts{'w'};
+}
 
+my $fasta_file = $opts{'f'};
 my %fasta = store_fasta($fasta_file);
 my %seq_hash = ();
 
-foreach my $id (keys %fasta){
-   if (exists $seq_hash{$fasta{$id}}){
-      $seq_hash{$fasta{$id}}++;
+my $i = 0;
+my $total = 0;
+ACC: foreach my $id (keys %fasta){
+   ++$total;
+   my $seq = $fasta{$id};
+   if (exists $seq_hash{$seq}){
+      next ACC;
    } else {
-      $seq_hash{$fasta{$id}} = 1;
+      $seq_hash{$seq} = 1;
+      ++$i;
+      print ">$i\n";
+      wrap_seq($seq, $wrap_len);
    }
 }
 
-foreach my $seq (sort {$seq_hash{$b} <=> $seq_hash{$a}} keys %seq_hash){
-   print "$seq\t$seq_hash{$seq}\n";
+warn("Unique: $i\nTotal: $total\n");
+
+sub wrap_seq {
+   my ($seq, $wrap_len) = @_;
+   for(my $i = 0; $i <length($seq); $i+=$wrap_len){
+      my $ss = substr($seq, $i, $wrap_len);
+      print "$ss\n";
+   }
 }
 
 sub store_fasta {
@@ -58,6 +77,7 @@ print STDERR <<EOF;
 Usage: $0 -f file
 
 Where:   -f         FASTA file
+         -w         length to wrap FASTA sequence (optional, default: 60)
          -h         this helpful usage message
 
 EOF
