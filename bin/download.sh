9c065ec2b74d584836c186dd55a5a41fa93f07eb
Dave Tang <davetingpongtang@gmail.com>
2021-12-17 17:36:16 +0900

    Do not include binaries; download them

---
+++ bin/download.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+os=$(uname -s)
+arc=$(arch)
+path=$(dirname $0)
+
+if [[ ${os} == Darwin ]]; then
+   mkdir -p ${path}/macos
+   wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/mac/datasets -O ${path}/macos/datasets && chmod 755 ${path}/macos/datasets
+   wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/mac/dataformat -O ${path}/macos/dataformat && chmod 755 ${path}/macos/dataformat
+elif [[ ${os} == Linux ]]; then
+   if [[ ${arc} == x86_64 ]]; then
+      mkdir -p ${path}/ubuntu
+      wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets -O ${path}/ubuntu/datasets && chmod 755 ${path}/ubuntu/datasets
+      wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/dataformat -O ${path}/ubuntu/dataformat && chmod 755 ${path}/ubuntu/dataformat
+   elif [[ ${arc} =~ arm ]]; then
+      mkdir -p ${path}/arm32
+      wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-arm/datasets -O ${path}/arm32/datasets && chmod 755 ${path}/arm32/datasets
+      wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-arm/dataformat -O ${path}/arm32/dataformat && chmod 755 ${path}/arm32/dataformat
+   fi
+else
+   >&2 echo Unrecognised operating system
+   exit 1
+fi
+
+>&2 echo Done
+exit 0
+
